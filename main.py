7cd0dca625f28ec12130a00228b9a1e3e38c45a1
EricChenZiYu <ziyucrc@gmail.com>
2025-06-08 17:55:03 +0800

    Enhance main.py and update dependencies for improved functionality
    - Added ChatOpenAI and StdioServerParameters to main.py for enhanced server interaction.
    - Updated pyproject.toml to include isort as a dependency.
    - Modified uv.lock to reflect the addition of isort version 6.0.1.
    - Added a blank line in weather_server.py for code clarity.

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import asyncio
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
-print(os.getenv("OPENAI_API_KEY"))
+load_dotenv()
+llm = ChatOpenAI()
+
+server_params = StdioServerParameters(
+    command="python",
+    # Make sure to update to the full absolute path to your math_server.py file
+    args=["/Users/xingjiabin/Documents/dev_env/mcp/mcp_clients/langchain-adapter/servers/math_server.py"],
+)
+
 
 async def main():
     print("Hello from langchain-adapter!")
